63837bd4c4f57677974747c63f8708c43c40e67c
Laura Luidolt <laura.luidolt@gmail.com>
2017-12-25 12:03:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({ [key]: value }, object);
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
